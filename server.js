16ef02c2a3869b428f2b2f12b500a9d3f04ea6e0
Onyeabor Ashikodi <ashikodiemeka@gmail.com>
2025-07-30 19:30:46 -0400

    add vercel json

--- server.js
+++ server.js
@@ -47,5 +47,5 @@ app.all("*", remixHandler);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () =>
-  console.log(`Express server listening at http://localhost:${port}`)
+  console.log(`Express server listening at port:${port}`)
 );
